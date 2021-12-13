5d81620dfcb31d659473f30f12957539d3e8ae35
mateo951 <mateovillagomez1995@gmail.com>
2021-12-13 16:51:32 -0500

    Add method for recent posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  has_many :post, dependent: :destroy
+  has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def recent_posts()
+    posts.limit(3)
+  end
 end
