689b1913a65b6518f14f405116cecf7ec25c386f
mateo951 <mateovillagomez1995@gmail.com>
2021-12-20 17:20:46 -0500

    Change routes syntax for simpler form understanging

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    @user = current_user
     @posts = @user.recent_posts
   end
 
   def show
-    @user = User.find(params[:user_id])
+    @user = current_user
     @post = @user.posts.find(params[:id])
     @comments = @post.comments.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'users#index'
-    resources :users, only: [:index, :show] do
-      resources :posts, only: [:index, :show]
-    end
+  root to: "users#index" # Specifies the root for the app
+  get "/users/", to: "users#index"
+  get "/users/:id", to: "users#show"
+  get "/users/:id/posts", to: "posts#index"
+  get "/users/:id/posts/:id", to: "posts#show"
 end
