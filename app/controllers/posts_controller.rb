b5737d7f1f50985efcd6ff0a8731394ad4f769e0
mateo951 <mateovillagomez1995@gmail.com>
2021-12-21 15:44:41 -0500

    Rework the app navegation & add changes to controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,36 @@
 class PostsController < ApplicationController
   def index
-    @user = current_user
-    @posts = @user.recent_posts
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 
   def show
-    @user = current_user
-    @post = @user.posts.find(params[:id])
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
     @comments = @post.comments.all
+  end
+
+  def new
+    @user = current_user
+    @post = Post.new
+    respond_to do |format|
+      format.html { render :new }
+    end
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.create(post_params)
+    if @post.save
+      flash[:success] = 'Post saved successfully'
+      redirect_to "/users/#{@post.author_id}/posts"
+    else
+      flash.now[:error] = 'Error: Post could not be saved'
+      render :new, locals: { post: @post }
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  root to: "users#index" # Specifies the root for the app
+  root to: "users#index"
   get "/users/", to: "users#index"
   get "/users/:id", to: "users#show"
   get "/users/:id/posts", to: "posts#index"
-  get "/users/:id/posts/:id", to: "posts#show"
+  get '/users/:id/posts/new', to: 'posts#new', as: 'new_post' # new
+  get "/users/:user_id/posts/:post_id", to: "posts#show"
+
+  post '/users/:id/posts/new', to: 'posts#create' # create
+
 end
