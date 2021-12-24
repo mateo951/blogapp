4f10e8c2730f5da91b2210df75f4fa3ef0add6ef
mateo951 <mateovillagomez1995@gmail.com>
2021-12-24 00:13:01 -0500

    Add unit testing for invalid inputs on posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: 'Mateo', photo: 'photo', bio: 'bio', posts_counter: 0) }
+
+  describe 'Invalid input' do
+    before :each do
+      subject.save
+    end
+
+    it 'name should be present' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'posts_counter must be a integer' do
+      subject.posts_counter = 'sadasdsa'
+      expect(subject).to_not be_valid
+    end
+
+    it 'posts-counter greater than or equal to 0' do
+      subject.posts_counter = -2
+      expect(subject).to_not be_valid
+    end
+  end
 end
