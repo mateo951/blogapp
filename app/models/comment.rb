f6cea4be8191db68b81ad798a26563e02d84b86d
mateo951 <mateovillagomez1995@gmail.com>
2021-12-23 19:49:07 -0500

    Fix issue with flash messages for comment and likes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user, foreign_key: :author_id
   belongs_to :post, foreign_key: :post_id
 
+  validates :text, presence: true
+
   def self.update_counter(post)
     post.update(comments_counter: (post.comments_counter + 1))
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :user, foreign_key: :author_id
   belongs_to :post, foreign_key: :post_id
-
+  
   def self.update_counter(post)
     post.update(likes_counter: (post.likes_counter + 1))
   end
