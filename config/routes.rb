3464c303239006ebeb4592ecdea3de76b36ff445
mateo951 <mateovillagomez1995@gmail.com>
2022-01-04 14:37:01 -0500

    Add fix for devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  root to: "home#index"
-  get 'home/index'
-  devise_for :views
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   get "/users/", to: "users#index"
   get "/users/:id", to: "users#show"
   get "/users/:id/posts", to: "posts#index"
@@ -10,8 +12,10 @@ Rails.application.routes.draw do
   get "/users/:user_id/posts/:post_id", to: "posts#show"
   get '/users/:user_id/posts/:post_id', to: 'comments#new', as: 'new_comment' # new
   get '/users/:user_id/posts/:post_id/new_like', to: 'likes#new', as: 'new_like' # new
-
+  
   post '/users/:id/posts/new', to: 'posts#create' # create
   post '/users/:user_id/posts/:post_id', to: 'comments#create' # create
   post '/users/:user_id/posts/:post_id/new_like', to: 'likes#create'
+
+  root to: "users#index"
 end
