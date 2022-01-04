f2fb9fd7dd4f82c6f5bb24a919c3c3b622a2bdfc
mateo951 <mateovillagomez1995@gmail.com>
2022-01-03 20:30:08 -0500

    Add views and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+  get 'home/index'
+  devise_for :views
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/users/", to: "users#index"
+  get "/users/:id", to: "users#show"
+  get "/users/:id/posts", to: "posts#index"
+  get '/users/:id/posts/new', to: 'posts#new', as: 'new_post' # new
+  get "/users/:user_id/posts/:post_id", to: "posts#show"
+  get '/users/:user_id/posts/:post_id', to: 'comments#new', as: 'new_comment' # new
+  get '/users/:user_id/posts/:post_id/new_like', to: 'likes#new', as: 'new_like' # new
+
+  post '/users/:id/posts/new', to: 'posts#create' # create
+  post '/users/:user_id/posts/:post_id', to: 'comments#create' # create
+  post '/users/:user_id/posts/:post_id/new_like', to: 'likes#create'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_04_011521) do
+ActiveRecord::Schema.define(version: 2022_01_04_011724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,18 @@ ActiveRecord::Schema.define(version: 2022_01_04_011521) do
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "views", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_views_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "posts"
