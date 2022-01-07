01f283feb006911eb3aa9b262d63d69af1711462
mateo951 <mateovillagomez1995@gmail.com>
2022-01-07 17:57:43 -0500

    Add extra configuration for JWT

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < Devise::SessionsController 
+    respond_to :json
+    
+    private
+    def respond_with(resource, _ops = {})
+        render json: resource
+    end
+    def respond_to_on_destroy
+        head :no_content
+    end
+end
