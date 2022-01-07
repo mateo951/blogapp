b96ce4068fa45d7a09d5cfd0e35e7c3e2c75c562
mateo951 <mateovillagomez1995@gmail.com>
2022-01-07 18:01:33 -0500

    Add fix for linters

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,11 @@
-class RegistrationsController < Devise::RegistrationsController 
-    respond_to :json
-    
-    private
-    def create
-        build_resource(sign_up_params)
-        resource.save
-        render json: resource, status: :created
-    end
+class RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  private
+
+  def create
+    build_resource(sign_up_params)
+    resource.save
+    render json: resource, status: :created
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @users = User.all
 
     respond_to do |format|
-      format.json { render :json => @users }
+      format.json { render json: @users }
     end
   end
 
