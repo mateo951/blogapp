5449b3d584530f7bab9ad26349709d7ed3d7edf4
Felipeg005 <juanfelipegomeztobon@gmail.com>
2022-01-07 16:46:50 -0500

    Add JWT gem, configure app for API, and config controllers and models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
