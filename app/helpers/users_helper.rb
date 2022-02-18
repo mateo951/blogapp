5e2d80d8a7e78a6c63da29f83c6ed78f05baac5d
mateo951 <mateovillagomez1995@gmail.com>
2022-02-18 16:09:05 -0500

    Solve merging issues

--- app/helpers/users_helper.rb
+++
@@ -1,5 +0,0 @@
-module UsersHelper
-  def get_photo(user)
-    user.photo.present? ? user.photo : "https://ui-avatars.com/api/?name=#{user.name}&background=random"
-  end
-end
--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  root 'users#index'
-    resources :users, only: [:index, :show] do
-      resources :posts, only: [:index, :show]
-    end
-end
